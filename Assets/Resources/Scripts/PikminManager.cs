0181a83626334ad31b34946695cef4d2faeb59be
WaniFi <chaos0824@naver.com>
2020-06-20 22:42:16 +0900

    Pikmin과 관련된 스크립트 및 오브젝트를 총괄하는 스크립트 구현

---
+++ Assets/Resources/Scripts/PikminManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PikminManager : MonoBehaviour
+{
+    [SerializeField] private Transform visualCylinder;
+    private PikminController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<PikminController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            SetWhistleCylinder(true);
+
+        if (Input.GetMouseButtonUp(0))
+            SetWhistleCylinder(false);
+    }
+
+    public void SetWhistleCylinder(bool on)
+    {
+        if(on)
+        {
+            visualCylinder.localScale = Vector3.zero;
+            visualCylinder.DOScaleX(4, .4f);
+            visualCylinder.DOScaleZ(4, .4f);
+            visualCylinder.DOScaleY(2, .3f).SetDelay(.4f);
+        }
+        else
+        {
+            visualCylinder.DOKill();
+            visualCylinder.DOScaleX(0, .15f);
+            visualCylinder.DOScaleZ(0, .15f);
+            visualCylinder.DOScaleY(0, .05f);
+        }
+    }
+}
