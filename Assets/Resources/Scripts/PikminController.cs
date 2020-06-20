231c582fd37f876a161eb15672f85c9869872bab
WaniFi <chaos0824@naver.com>
2020-06-20 22:41:44 +0900

    Pikmin 오브젝트를 컨트롤을 관리하는 스크립트 구현

---
+++ Assets/Resources/Scripts/PikminController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PikminController : MonoBehaviour
+{
+    [HideInInspector] public Vector3 HitPoint = Vector3.zero;
+    [SerializeField] private Vector3 targetOffset = Vector3.zero;
+    [SerializeField] private Transform ControlObject;
+
+    private Camera camera = default;
+    private int colliderLayer;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+        colliderLayer = -257;
+    }
+
+    private void Update()
+    {
+        UpdateMousePosition();
+    }
+
+    private void UpdateMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 1000, colliderLayer))
+        {
+            HitPoint = hit.point;
+            ControlObject.position = HitPoint + targetOffset;
+            ControlObject.up = Vector3.Lerp(ControlObject.up, hit.normal, .3f);
+        }
+    }
+}
