0df01c64020ce2bcbc9d60c6a6343bf5a2dadb63
WaniFi <chaos0824@naver.com>
2020-06-21 23:07:19 +0900

    Pikmin Controller의 Cylinder 크기 조절 값 수정

--- Assets/Resources/Scripts/PikminManager.cs
+++ Assets/Resources/Scripts/PikminManager.cs
@@ -6,11 +6,13 @@ using DG.Tweening;
 public class PikminManager : MonoBehaviour
 {
     [SerializeField] private Transform visualCylinder;
+    [SerializeField] private Pikmin pikminPrefab;
     private PikminController controller;
 
     private void Awake()
     {
         controller = GetComponent<PikminController>();
+        InitializePikmin();
     }
 
     // Update is called once per frame
@@ -30,7 +32,7 @@ public class PikminManager : MonoBehaviour
             visualCylinder.localScale = Vector3.zero;
             visualCylinder.DOScaleX(4, .4f);
             visualCylinder.DOScaleZ(4, .4f);
-            visualCylinder.DOScaleY(2, .3f).SetDelay(.4f);
+            visualCylinder.DOScaleY(0.7f, .3f).SetDelay(.2f);
         }
         else
         {
@@ -39,5 +41,12 @@ public class PikminManager : MonoBehaviour
             visualCylinder.DOScaleZ(0, .15f);
             visualCylinder.DOScaleY(0, .05f);
         }
+    }
+
+    private void InitializePikmin()
+    {
+        PikminSpawner[] spawners = FindObjectsOfType(typeof(PikminSpawner)) as PikminSpawner[];
+        foreach (PikminSpawner spawner in spawners)
+            spawner.SpawnPikmin(pikminPrefab);
     }
 }
