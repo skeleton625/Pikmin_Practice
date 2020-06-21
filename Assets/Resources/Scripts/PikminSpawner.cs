b4aa7c3a24a02da52af3642d9096b915b31202e1
WaniFi <chaos0824@naver.com>
2020-06-21 23:11:33 +0900

    Pikmin 소환 스크립트 구현

---
+++ Assets/Resources/Scripts/PikminSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PikminSpawner : MonoBehaviour
+{
+    [SerializeField] private int spawnNum = 1;
+    [SerializeField] private float spawnRadius;
+
+    public void SpawnPikmin(Pikmin pikmin)
+    {
+        for(int i = 0; i < spawnNum; i++)
+        {
+            Pikmin newPikmin = Instantiate(pikmin);
+            Vector3 pos = transform.position + (Random.insideUnitSphere * spawnRadius);
+            pos.y = 0;
+            newPikmin.transform.position = pos;
+        }
+    }
+}
