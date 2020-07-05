f1fcba8721b6c9233fe968e077e671985a20cbd9
WaniFi <chaos0824@naver.com>
2020-07-05 23:24:37 +0900

    게임 총괄 관리 스크릡트 구현

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private PikminManager Pmanager;
+    [SerializeField] private PikminController Pcontrol;
+    [SerializeField] private CameraMovement Cmovement;
+
+    private void Update()
+    {
+        Cmovement.RotateCamera();
+        Pmanager.ControlPikmin();
+        Pcontrol.UpdateMousePosition();
+    }
+}
