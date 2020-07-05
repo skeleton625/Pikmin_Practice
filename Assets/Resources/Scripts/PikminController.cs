e66e5fa47326f0c9ce7cd0a27f08d56bcc987114
WaniFi <chaos0824@naver.com>
2020-07-05 23:23:59 +0900

    일부 코드 수정

--- Assets/Resources/Scripts/PikminController.cs
+++ Assets/Resources/Scripts/PikminController.cs
@@ -11,33 +11,28 @@ public class PikminController : MonoBehaviour
     [SerializeField] private LineRenderer TargetLine;
 
     private int colliderLayer;
+    private Camera camera = default;
     private readonly int linePointsCount = 5;
 
-    private Camera camera = default;
     private void Awake()
     {
-        camera = Camera.main;
+        /* Layer Collider 제외 숫자 */
         colliderLayer = -257;
+        camera = FindObjectOfType<Camera>();
         TargetLine.positionCount = linePointsCount;
     }
 
-    private void Update()
-    {
-        UpdateMousePosition();
-    }
-
-    private void UpdateMousePosition()
+    public void UpdateMousePosition()
     {
         RaycastHit hit;
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hit, 1000, colliderLayer))
+        if (Physics.Raycast(ray, out hit, 1000, colliderLayer))
         {
             HitPoint = hit.point;
             Target.position = HitPoint + targetOffset;
             Target.up = Vector3.Lerp(Target.up, hit.normal, .3f);
 
-            float lerpValue = 1f / linePointsCount;
-            for(int i = 0; i < linePointsCount; i++)
+            for (int i = 0; i < linePointsCount; i++)
             {
                 Vector3 linePos = Vector3.Lerp(Follower.position, Target.position, i * linePointsCount);
                 TargetLine.SetPosition(i, linePos);
