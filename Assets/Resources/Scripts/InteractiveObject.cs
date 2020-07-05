edcb4b00a3af9c77a863762b48c3dfe22066c11e
WaniFi <chaos0824@naver.com>
2020-07-05 23:25:02 +0900

    Pikmin과 상호작요하는 오브젝트의 관리 스크립트 구현 중

---
+++ Assets/Resources/Scripts/InteractiveObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class InteractiveObject : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField] private int NeededPikminCount = 1;
+    [SerializeField] private float InteractiveRadius = 1;
+    private int currentPikminCount = 0;
+
+    public void AssignPikmin()
+    {
+        ++currentPikminCount;
+
+        /* 개수가 모두 채워질 경우, 초과할 경우에 대한 결과 구현 필요 */
+        if (currentPikminCount.Equals(NeededPikminCount)) ;
+
+        else if (currentPikminCount > NeededPikminCount) ;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return Vector3.zero;
+    }
+}
