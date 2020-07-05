562c98d2d4e0d09b4b8c24764db057459bd48dbd
WaniFi <chaos0824@naver.com>
2020-07-05 23:26:52 +0900

    Pikmin의 Agent 도착 여부 파악 함수 구현

---
+++ Assets/Resources/Scripts/PikminUtility.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.AI;
+
+/* 이동이 완료되었는지 확인하기위한 NavMeshAgent 전역 함수용 클래스 */
+public static class PikminUtility
+{
+    public static bool IsDone(this NavMeshAgent agent)
+    {
+        return (!agent.pathPending &&
+                 agent.remainingDistance <= agent.stoppingDistance &&
+                (agent.hasPath || agent.velocity.sqrMagnitude.Equals(0))
+               );
+    }
+}
