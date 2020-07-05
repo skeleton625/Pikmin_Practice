2e6b87cca2cc3a7851c8ce6e1ed6e11d036940d8
WaniFi <chaos0824@naver.com>
2020-07-05 23:24:18 +0900

    카메라 이동 관리 스크립트 구현

---
+++ Assets/Resources/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using DG.Tweening;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera virtualCam;
+    [SerializeField] private float CameraRotValue = 90;
+    [SerializeField] private float RotTime = .5f;
+
+    private CinemachineOrbitalTransposer transposer;
+
+    private void Start()
+    {
+        transposer = virtualCam.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+    }
+
+    private void SetCameraAxis(float x)
+    {
+        transposer.m_XAxis.Value = x;
+    }
+
+    public void RotateCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            DOVirtual.Float(transposer.m_XAxis.Value, transposer.m_XAxis.Value + CameraRotValue, RotTime, SetCameraAxis).SetEase(Ease.OutSine);
+        if (Input.GetKeyDown(KeyCode.E))
+            DOVirtual.Float(transposer.m_XAxis.Value, transposer.m_XAxis.Value - CameraRotValue, RotTime, SetCameraAxis).SetEase(Ease.OutSine);
+    }
+}
