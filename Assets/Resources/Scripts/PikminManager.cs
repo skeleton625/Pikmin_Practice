a86032773477842bd1ce7e4a765893716df8af2c
WaniFi <chaos0824@naver.com>
2020-10-24 17:19:36 +0900

    코드 최적화 진행

--- Assets/Resources/Scripts/PikminManager.cs
+++ Assets/Resources/Scripts/PikminManager.cs
@@ -15,7 +15,7 @@ public class PikminManager : MonoBehaviour
     [SerializeField] private Pikmin pikminPrefab;
 
     private PikminController controller;
-    private Queue<Pikmin> pikminQueue;
+    private List<Pikmin> pikminList;
 
     public static PikminManager instance;
 
@@ -28,7 +28,7 @@ public class PikminManager : MonoBehaviour
 
     private void InitializePikmin()
     {
-        pikminQueue = new Queue<Pikmin>();
+        pikminList = new List<Pikmin>();
         controller = GetComponent<PikminController>();
 
         PikminSpawner[] spawners = FindObjectsOfType(typeof(PikminSpawner)) as PikminSpawner[];
@@ -38,7 +38,7 @@ public class PikminManager : MonoBehaviour
 
     public void GetPikmin(Pikmin pikmin)
     {
-        pikminQueue.Enqueue(pikmin);
+        pikminList.Add(pikmin);
     }
 
     public void SetWhistleCylinder(bool on)
@@ -70,14 +70,21 @@ public class PikminManager : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (pikminQueue.Count < 1)
+            if (pikminList.Count < 1)
                 return;
 
-            Pikmin pikmin = pikminQueue.Dequeue();
-
-            pikmin.agent.enabled = false;
-            pikmin.transform.DOMove(pikminThrowPosition.position, .05f);
-            pikmin.Throw(controller.HitPoint, .5f, .05f);
+            for(int i = 0; i < pikminList.Count; i++)
+            {
+                if(pikminList[i].agent.IsDone())
+                {
+                    Pikmin pikmin = pikminList[i];
+                    pikminList.RemoveAt(i);
+                    pikmin.agent.enabled = false;
+                    pikmin.transform.DOMove(pikminThrowPosition.position, .05f);
+                    pikmin.Throw(visualCylinder.position, .5f, .05f);
+                    break;
+                }
+            }
             
             /* Pikmin 던지는 소리, 움직이는 소리 필요 */
         }
