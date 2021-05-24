402c185c5129a71e5fdce7251d92d6687f2604bc
Skeleton <chaos0824@naver.com>
2021-05-24 21:32:11 +0900

    도착한 뒤, Pikmin을 항당해 생기는 오류 수정

--- Assets/Resources/Scripts/CarryObject.cs
+++ Assets/Resources/Scripts/CarryObject.cs
@@ -5,33 +5,43 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class CarryObject : InteractiveObject
 {
-    [SerializeField] private Camera mainCamera;
-    [SerializeField] private Transform Destination;
+    [SerializeField] private Transform Destination = null;
+
+    private float agentSpeed = 0f;
+    private Camera mainCamera = null;
     private NavMeshAgent agent = null;
     private Coroutine carryingCoroutine = null;
-    private float originalAgnetSpeed;
 
     public override void Initialize()
     {
         base.Initialize();
         agent = GetComponent<NavMeshAgent>();
-        originalAgnetSpeed = agent.speed;
+        agentSpeed = agent.speed;
+        mainCamera = Camera.main;
     }
 
     public override void Interact()
     {
+        if (agent.IsArrived(transform.position, Destination.position))
+            return;
+
         if (agent.enabled)
             agent.isStopped = false;
         else
+        {
+            agent.isStopped = false;
             agent.enabled = true;
+        }
+
         if (carryingCoroutine != null)
             StopCoroutine(carryingCoroutine);
-        StartCoroutine(GetInPosition());
+        carryingCoroutine = StartCoroutine(GetInPosition());
 
         IEnumerator GetInPosition()
         {
             agent.SetDestination(Destination.position);
-            yield return new WaitUntil(() => agent.IsDone());
+            while (!agent.IsDone())
+                yield return null;
             agent.enabled = false;
         }
     }
@@ -41,11 +51,15 @@ public class CarryObject : InteractiveObject
         if(agent.enabled)
             agent.isStopped = true;
         if (carryingCoroutine != null)
+        {
             StopCoroutine(carryingCoroutine);
+            carryingCoroutine = null;
+        }
     }
 
     private void Update()
     {
+        /* 할당된 Pikmin 수 표시 UI */
         if (fractionObject != null)
             fractionObject.transform.position = mainCamera.WorldToScreenPoint(transform.position + uiOffset);
     }
