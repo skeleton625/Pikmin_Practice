44700a53a3b802df6ad92145f3ea3c04ba3da8a7
Skeleton <chaos0824@naver.com>
2021-05-24 21:29:11 +0900

    도착 여부 확인 전역 함수 구현

--- Assets/Resources/Scripts/PikminUtility.cs
+++ Assets/Resources/Scripts/PikminUtility.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine.AI;
+using UnityEngine;
 
 /* 이동이 완료되었는지 확인하기위한 NavMeshAgent 전역 함수용 클래스 */
 public static class PikminUtility
@@ -6,8 +7,12 @@ public static class PikminUtility
     public static bool IsDone(this NavMeshAgent agent)
     {
         return (!agent.pathPending &&
-                 agent.remainingDistance <= agent.stoppingDistance &&
-                (agent.hasPath || agent.velocity.sqrMagnitude.Equals(0))
-               );
+                 agent.remainingDistance <= agent.stoppingDistance);
+    }
+
+    public static bool IsArrived(this NavMeshAgent agent, Vector3 start, Vector3 dest)
+    {
+        start.y = dest.y = 0f;
+        return Vector3.Distance(start, dest) <= agent.stoppingDistance;
     }
 }
