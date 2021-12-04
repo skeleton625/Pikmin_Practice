dc51f22005ee136fe0e07fcc3a9353f1cb40e915
Skeleton <chaos0824@naver.com>
2021-12-04 20:44:43 +0900

    이동 오브젝트 Manager 추가

---
+++ Assets/Resources/Scripts/CarryObjectManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarryObjectManager : MonoBehaviour
+{
+    [Header("Source Component"), Space(10)]
+    [SerializeField] private Transform Source = null;
+
+    [Header("Destination Component"), Space(10)]
+    [SerializeField] private Transform Dsetination = null;
+
+    [Header("Carry Object Component"), Space(10)]
+    [SerializeField] private Transform SmallCarryObject = null;
+    [SerializeField] private float CarryObjectScale = 0f;
+    [SerializeField] private float SpawnRadius = 0f;
+
+    public void FinishCarryObject(CarryObject carryObject)
+    {
+        carryObject.transform.localScale = new Vector3(CarryObjectScale, CarryObjectScale, CarryObjectScale);
+
+        var position = Source.position + Random.insideUnitSphere * SpawnRadius;
+        position.y = 0;
+
+        carryObject.Initialize();
+        carryObject.transform.position = position;
+    }
+}
